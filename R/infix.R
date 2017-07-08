47537e7bbf4ba3b4b122309bace9539baba80781
Zebulun Arendsee <arendsee@iastate.edu>
2017-07-08 15:47:34 -0500

    Refactor vignettes into multiple linked files

--- R/infix.R
+++ R/infix.R
@@ -28,65 +28,6 @@ NULL
 
 #' @rdname infix
 #' @export
-`%>^%` <- function(lhs, rhs) {
-  envir <- parent.frame()
-  cmd   <- list(bind, substitute(lhs), substitute(rhs), io_combine=branch_combine)
-  eval(as.call(cmd), envir=envir)
-}
-
-#' @rdname infix
-#' @export
-`%|>%` <- function(lhs, rhs) {
-  envir <- parent.frame()
-  bind_if <- function(m) { ! m_OK(m) }
-  cmd   <- list(bind, substitute(lhs), substitute(rhs), bind_if=bind_if)
-  eval(as.call(cmd), envir=envir)
-}
-
-#' @rdname infix
-#' @export
-`%||%` <- function(lhs, rhs) {
-  envir <- parent.frame()
-  rhs_str <- deparse(substitute(rhs))
-  cmd   <- list(
-    bind,
-    substitute(lhs),
-    substitute(rhs),
-    # skip the bind code
-    bind_if = .false,
-    # and instead just evaluate the rhs
-    bind_else = function(i,o) mrun(o, desc=rhs_str),
-    # if the lhs failed, pass the evaluated rhs
-    emit = function(i,o) if(m_OK(i)){ i } else { o }
-  )
-  eval(as.call(cmd), envir=envir)
-}
-
-
-#' @rdname infix
-#' @export
-`%^>%` <- function(lhs, rhs) {
-  .bind_if_fb <- function(m){
-    m_branch(m) %>% sapply(m_OK) %>% all
-  }
-  .bind_args_fb <- function(m){
-    m_branch(m) %>% lapply(m_value)
-  }
-
-  cmd   <- list(
-    bind,
-    lhs,
-    substitute(rhs),
-    bind_args = .bind_args_fb,
-    bind_if   = .bind_if_fb
-  )
-  envir <- parent.frame()
-  eval(as.call(cmd), envir=envir)
-
-}
-
-#' @rdname infix
-#' @export
 `%*>%` <- function(lhs, rhs) {
   envir <- parent.frame()
   lexp <- as.list(substitute(lhs))[-1]
@@ -117,6 +58,69 @@ NULL
     substitute(lhs),
     substitute(rhs),
     io_combine = bypass_combine
+  )
+  eval(as.call(cmd), envir=envir)
+}
+
+#' @rdname infix
+#' @export
+`%>^%` <- function(lhs, rhs) {
+  envir <- parent.frame()
+  cmd   <- list(bind, substitute(lhs), substitute(rhs), io_combine=branch_combine)
+  eval(as.call(cmd), envir=envir)
+}
+
+#' @rdname infix
+#' @export
+`%^>%` <- function(lhs, rhs) {
+  .bind_if_fb <- function(m){
+    m_branch(m) %>% sapply(m_OK) %>% all
+  }
+  .bind_args_fb <- function(m){
+    m_branch(m) %>% lapply(m_value)
+  }
+
+  cmd   <- list(
+    bind,
+    lhs,
+    substitute(rhs),
+    bind_args = .bind_args_fb,
+    bind_if   = .bind_if_fb
+  )
+  envir <- parent.frame()
+  eval(as.call(cmd), envir=envir)
+
+}
+
+#' @rdname infix
+#' @export
+`%|>%` <- function(lhs, rhs) {
+  envir <- parent.frame()
+  bind_if <- function(m) { ! m_OK(m) }
+  cmd   <- list(bind, substitute(lhs), substitute(rhs), bind_if=bind_if)
+  eval(as.call(cmd), envir=envir)
+}
+
+
+# ----------------------------------------------------
+# The following have values, not functions, on the rhs
+# ----------------------------------------------------
+
+#' @rdname infix
+#' @export
+`%||%` <- function(lhs, rhs) {
+  envir <- parent.frame()
+  rhs_str <- deparse(substitute(rhs))
+  cmd   <- list(
+    bind,
+    substitute(lhs),
+    substitute(rhs),
+    # skip the bind code
+    bind_if = .false,
+    # and instead just evaluate the rhs
+    bind_else = function(i,o) mrun(o, desc=rhs_str),
+    # if the lhs failed, pass the evaluated rhs
+    emit = function(i,o) if(m_OK(i)){ i } else { o }
   )
   eval(as.call(cmd), envir=envir)
 }
