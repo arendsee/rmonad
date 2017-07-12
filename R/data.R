8f26d05f65f880359d080ce397a989ba45adffb4
Zebulun Arendsee <arendsee@iastate.edu>
2017-07-12 09:55:52 -0500

    Wrote a draft of a GFF processing vignette
    I also found a bug. When using the `esc` function, a warning that occurs
    late in the pipeline is printed for every prior node, in a sort of weird
    back-propagation. The `mtabulate` and `missues` results looks correct,
    though. So I think the problem is limited to the `esc` function.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Data for GFF processing vignette
+#'
+#' Contains 4 files:
+#' \enumerate{
+#'     \item synmap - hand made synteny map
+#'     \item qgff   - set of intervals relative to the query
+#'     \item tgff   - set of intervals relative to the target
+#'     \item hitmap - map to filter through the main synteny map
+#'     \item qgen   - lengths of query contigs/scaffolds/chromosomes
+#'     \item tgen   - lengths of target contigs/scaffolds/chromosomes
+#' }
+#' 
+#' @format List
+"gff_str"
