1c972f33a7155d45a68d0bc08d5fc99a632ef6ae
Zebulun Arendsee <arendsee@iastate.edu>
2017-07-22 11:38:08 -0500

    Add simple nesting test and bump dev version
    I'll add more comprehensive tests once I nail down the functionallity I
    want.

---
+++ tests/testthat/test-nest.R
@@ -0,0 +1,22 @@
+context("pipeline nesting")
+
+
+addit <- function(x,y) x + y
+
+subit <- function(x,y) x - y
+
+foo <- function(x,k=3){
+  45 %>>% addit(x) %>>% subit(k)
+}
+
+test_that("Nesting works for named functions", {
+  expect_equal(4 %>>% foo %>% esc, 46)
+  expect_equal(
+    4 %>>% foo %>% sapply(m_code),
+    c("4", "45", "addit(x)", "subit(k)", "foo")
+  )
+  expect_equal(
+    4 %>>% foo %>% lapply(m_parents) %>% sapply(length),
+    c(0,0,2,1,1)
+  )
+})
