fe197a4fd2b8244ec97cfe7e796785a2328f5067
Zebulun Arendsee <arendsee@iastate.edu>
2017-08-21 04:59:35 -0500

    Fixed the strange problem (needed R6 clone)
    All tests pass, buth the GFF vignette is breaking now

--- R/side_monad-to-monad.R
+++ R/side_monad-to-monad.R
@@ -33,8 +33,7 @@ doc <- function(m, ...){
 #' @export
 unnest <- function(m){
   if(is_rmonad(m) && has_value(m) && is_rmonad(m_value(m))){
-
-    m_nest(m)  <- m_value(m)
+    m_nest(m) <- m_value(m)$clone()
     m_OK(m)    <- m_OK(m_nest(m))
     # move the value from the nest to the outer position
     m_value(m) <- m_value(m_nest(m))
