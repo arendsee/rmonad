83ebac8607a926a0315ecd8a224efe1e0e1a1f5e
Zebulun Arendsee <arendsee@iastate.edu>
2017-10-04 17:26:09 -0500

    Update data

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' Contains 4 files:
 #' \enumerate{
 #'   \item good - a valid GFF string
-#'   \item not_a_gff1 - a string that isn't a GFF file at all
+#'   \item not_a_gff1 - a string that is not a GFF file at all
 #'   \item invalid_type - a table with invalid types
 #'   \item good_result - the final pipeline produced using the good gff
 #' }
