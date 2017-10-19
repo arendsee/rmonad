eb8c7374afd3ea072cc0b79c31ec2c7d61037ada
Zebulun Arendsee <arendsee@iastate.edu>
2017-10-19 17:50:06 -0500

    Last stable commit before igraph refactor
    This commit adds some cache functions, which are not currently used.
    More importantly, it is the last stable commit before the great refactor
    I've been planning so long. I'll replace my current network system with
    igraph. This is a pretty complicated refactor; rmonad@dev will probably
    not be stable for a few days.
    
    Here is what I need to do:
    
     1. replace the Rmonad object - Use a simple S4 object with just the
     node specific information. The connections to other nodes will be
     handled by igraph.
    
     2. Rewrite all accessors - particularly tricky will be those related to
     links ("parent", "nest", "prior", "branch"). These relationships will
     be stored as edge attributes.
    
     3. Remove the R6 specific tripe (e.g. the `$clone` in `combine` and the
     `$new()` in `as_monad`)
    
     4. Rewrite `as.list`. This functions mediates most of the network
     access. Eventually it should become less important (since we can use
     igraph traversal functions), but for now making `as.list` work exactly
     as it did before will make the refactor MUCH easier.
    
     5. Overhaul magic - this may be a lot of work, I think I'll have to
     totally reimplement it. On the bright side, it may be much more elegant
     afterwards.
    
     6. Deprecate funnel, instead use something like `rcall(f, ...)`, which
     calls `f` on the inputs `...`. This would be like doing `funnel(...)
     %*>% f`, but it does not create the intermediate node with a list of
     inputs.
    
     7. Use cache functions rather than the current `value` slot. The
     default behavior will be to store in memory until the value has been
     used, whereupon it will be deleted (replaced with `cacheNone`). The
     current commit lays the groundwork for this.

---
+++ R/core_cache.R
@@ -0,0 +1,64 @@
+#' Represent a value that has not been set
+#'
+#' This is the default value of RmonadNode@value. It should always be replaced
+#' shortly after the object is created, thus should only be encountered if 1)
+#' the user is directly creating RmonadNode objects (in which case they should
+#' be spoken to sternly) or 2) there is a bug in rmonad.
+#'
+#' @return NULL
+voidCache <- function(){
+  warning("Attempting to access an a DataNode with no stored value, returning NULL")
+  NULL
+}
+
+#' Represent a value that has been deleted
+#'
+#' By default, the value of a node that has already been executed will be set
+#' to this function.
+#'
+#' @param warn logical Emit a warning on access 
+noCache <- function(warn=TRUE){
+  if(warn){
+    warning("Attempting to access data that has been deleted, returning NULL")
+  }
+  NULL
+}
+
+#' Store a value in memory
+#'
+#' @param x Value to be stored
+#' @examples
+#' foo <- 45
+#' foo_proxy <- memoryCache(foo)
+#' foo
+#' foo_proxy()
+memoryCache <- function(x){
+  function() x
+}
+
+#' Make a function of x that caches data locally
+#'
+#' @param path A directory in which to cache results
+#' @examples
+#' \dontrun{
+#'   foo <- 45
+#'   localCacher <- makeLocalCacher(".")
+#'   foo_ <- localCacher(45)
+#'   rm(foo)
+#'   foo_()
+#' }
+makeLocalCacher <- function(path){
+  # Save x and return a function that can load it
+  function(x){
+    filename <- file.path(path, paste0(uuid::UUIDgenerate(), ".Rdata"))
+    if(!dir.exists(path)){
+      dir.create(path, recursive=TRUE)
+    }
+    save(x, file=filename) 
+    rm(x)
+    function(){
+      load(filename)
+      x
+    }
+  }
+}
