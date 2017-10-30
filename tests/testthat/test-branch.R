e905640468b27c10232c2c6ed929c3f1d3aba822
Zebulun Arendsee <arendsee@iastate.edu>
2017-10-30 18:41:29 -0500

    Test for trans links when branching into nest
    Find a new bug

--- tests/testthat/test-branch.R
+++ tests/testthat/test-branch.R
@@ -42,3 +42,32 @@ test_that('New rmonad dont overwrite old ones (not an issue without R6)', {
     8
   )
 })
+
+
+# -----------------------------------------------------------------------------
+m <- 'a' %v>% paste0('b') %>^%
+{
+  paste0(., '-c') %v>%
+  paste0(   'd')  %v>%
+  paste0(   'e-')
+} %v>%
+paste0('f') %v>% paste0('g') %v>% paste0('h')
+# value   | depth | vid | pid | tid | nid | notes
+# a       | 1     |  1  |  -  |  -  |  -  |
+# ab      | 1     |  2  |  1  |  -  |  -  |
+# ab-c    | 2     |  3  |  -  |  2  |  -  | input to nest
+# ab-cd   | 2     |  4  |  3  |  -  |  -  |
+# NULL,   | 2     |  5  |  4  |  -  |  -  | output of nest (value sent to wrapper)
+# ab-cde- | 1     |  6  |  2  |  -  |  5  | the nest wrapper (holds output of nest)
+# abf     | 1     |  7  |  2  |  -  |  -  | first element of second branch
+# abfg    | 1     |  8  |  7  |  -  |  -  |
+# abfgh   | 1     |  9  |  8  |  -  |  -  |
+# vid - vertex id
+# did - dependent id
+# tid - transitive id
+# nid - nest id
+# =============================================================================
+test_that('Branching into nests correctly makes transitive links', {
+  expect_equal(get_value(m,F), list("a", "ab", "ab-c", "ab-cd", NULL, "ab-cde-", "abf", "abfg", "abfgh"))
+  expect_equal(get_dependents(m), list(2, c(3,6,7), 4, 5, integer(0), integer(0), 8, 9, integer(0)))
+})
