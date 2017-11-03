1baddb4d900116423fc65b2f6f5e1ddfae72e78f
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-03 03:50:02 -0500

    Added RmonadData object
    I feel like I am running in a circle here ...
    
    igraph simply is not good at handling attributes. So will wrap all the
    data associated with a node into a simgle object. These objects will
    collected into a named list. igraph will handle the network operations.
    These will be named graphs, which point to values in the RmonadData
    list.
    
    Here I only add an RmonadData class. What remains to be done is delete
    the abomination in graph and replace all `{set,get}.vertex*` functions
    with access to the list.

--- R/classes.R
+++ R/classes.R
@@ -12,7 +12,25 @@ setClass(
   "Rmonad",
   representation(
     graph = "igraph",
-    head = "character"
+    head = "character" #, data = "list"
     # TODO: add rmonad settings (e.g. default cache function)
+  )
+)
+
+setClass(
+  "RmonadData",
+  representation(
+    code       = "character",
+    error      = "character",
+    warnings   = "character",
+    notes      = "character",
+    OK         = "logical",
+    doc        = "character",
+    mem        = "integer",
+    time       = "numeric",
+    meta       = "list",
+    summary    = "list",
+    nest_depth = "integer",
+    stored     = "logical"
   )
 )
