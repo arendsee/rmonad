7588a982b326d8194b79e72b53c3074dc0cd0d09
Zebulun Arendsee <arendsee@iastate.edu>
2018-02-19 10:53:50 -0600

    Add tests for preservation of cache in tags

--- tests/testthat/test-tag.R
+++ tests/testthat/test-tag.R
@@ -15,7 +15,7 @@ test_that("tag sets tag", {
   )
 })
 
-m <- as_monad(16) %>% tag('a') %v>% sqrt %>% tag('b') %v>% sqrt
+m <- as_monad(16) %>% tag('a') %>>% sqrt %>% tag('b') %v>% sqrt
 test_that("view gets tagged node", {
   expect_equal(view(m, 'b') %>% esc, 4)
 })
@@ -28,7 +28,7 @@ m2 <- funnel(view(m, 'a'), view(m, 'b'), 99) %*>% sum
 test_that("view works in funnel", {
   expect_equal(esc(m2), 119)
   expect_equal(get_dependents(m2),    list(5L,   c(3L,5L), c(4L,5L), integer(0), 6L,   integer(0)))
-  expect_equal(get_value(m2, warn=F), list(NULL, NULL,     4,        2,          NULL, 119       ))
+  expect_equal(get_value(m2, warn=F), list(NULL, 16,       4,        2,          NULL, 119       ))
 })
 
 f <- make_recacher(memory_cache)
@@ -65,7 +65,16 @@ test_that("other get_* accessors work", {
 
 m4 <- 256 %>% f(c('a', 'b')) %>>% sqrt %>% f('b') %>>% sqrt %>% f(c('a', 'b'))
 m5 <- 256 %>% f('a') %>>% sqrt %>% f('b') %>>% sqrt %>% f('a')
-test_that("", {
+test_that("Can search nested tags", {
   expect_equal(names(get_value(m4, tag=c('a', 'b'))), c("a/b/1", "a/b/3"))
   expect_equal(names(get_value(m5, tag='a')), c("a/1", "a/3"))
+})
+
+f <- make_recacher(make_local_cacher())
+m6 <- 256 %>% f('a') %>>%
+  sqrt %>% f('b') %>% funnel(a = ., b=view(., 'a')) %*>%
+  sum
+test_that("Cached tags are preserved", {
+  expect_equal(get_tag(m6), list("a", "b", "", ""))
+  expect_equal(get_value(m6, warn=F), list(256, 16, NULL, 272))
 })
